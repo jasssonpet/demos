c4090bf94ff4c18f92dafef40ed8bda4fe5d4ea3
Jason Zhekov <jasssonpet@gmail.com>
2013-02-18 23:30:52 +0200

    Small fixes

--- scripts/fork.js
+++ scripts/fork.js
@@ -1,13 +1,15 @@
 (function() {
-    var username = location.hostname.split('.')[0]
+'use strict';
 
-      , parts = location.pathname.match(/\/(.+?)\/(.+)/)
+var username = location.hostname.split('.')[0]
 
-      , repo = parts[1]
-      , folder = parts[2]
+  , parts = location.pathname.match(/\/(.+?)\/(.+)/)
 
-      , source = 'https://github.com/' + username + '/' + repo +'/tree/gh-pages/' + folder
+  , repo = parts[1]
+  , folder = parts[2]
 
-    document.write('<a href="' + source + '"><img style="position: absolute; top: 0; right: 0; border: 0;"' +
-        ' src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"></a>')
+  , source = 'https://github.com/' + username + '/' + repo +'/tree/gh-pages/' + folder
+
+document.write('<a href="' + source + '"><img style="position: absolute; top: 0; right: 0; border: 0;"' +
+    ' src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"></a>')
 }())
