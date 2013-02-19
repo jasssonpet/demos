97e45d57c9f3ad0ef630ac168bb1d9c7468a65d7
Jason Zhekov <jasssonpet@gmail.com>
2013-02-20 01:38:18 +0200

    Small fixes

--- matrix-traversal/scripts/script.js
+++ matrix-traversal/scripts/script.js
@@ -3,90 +3,114 @@
 (function() {
 'use strict';
 
-var APP = angular.module('matrixApp', [])
-  , FPS = 1000 / 1.5
+var APP = angular.module('app', [])
+  , FPS = 1000 / 2 // Delay
 
-function isInside(matrix, row, col) {
-    return 0 <= row && row < matrix.length && 0 <= col && col < matrix[row].length
-}
-
-var getSampleMatrix = (function() {
-    var sampleMatrix =
-        [ [1, 3, 2, 2, 2, 4]
-        , [3, 3, 3, 2, 4, 4]
-        , [4, 3, 1, 2, 3, 3]
-        , [4, 3, 1, 3, 3, 1]
-        , [4, 3, 3, 3, 1, 1]
+  , sampleMatrix =
+    [ [1, 3, 2, 2, 2, 4]
+    , [3, 3, 3, 2, 4, 4]
+    , [4, 3, 1, 2, 3, 3]
+    , [4, 3, 1, 3, 3, 1]
+    , [4, 3, 3, 3, 1, 1]
     ]
 
-    return function() {
+APP.controller('ctrl', function($scope, $timeout) {
+    $scope.matrix = (function() {
         var matrix = []
 
         sampleMatrix.forEach(function(row) {
             matrix.push([])
 
             row.forEach(function(cell) {
-                matrix[matrix.length - 1].push(
-                    { value: cell
-                    , visited: false
-                    }
-                )
+                matrix[matrix.length - 1].push({ value: cell, visited: false })
             })
         })
 
         return matrix
-    }
-}())
-
-APP.controller('matrixController', function($scope, $timeout) {
-    function reset() {
-        $scope.matrix.forEach(function(row) {
-            row.forEach(function(cell) {
-                cell.visited = false
-            })
-        })
-    }
-
-    function isNext(row, col, value) {
-        if (!isInside($scope.matrix, row, col))
-            return false
-
-        if ($scope.matrix[row][col].visited)
-            return false
-
-        if ($scope.matrix[row][col].value != value)
-            return false
-
-        return true
-    }
-
-    function makeNext(row, col) {
-        return function() {
-            BFS(row, col)
-        }
-    }
-
-    var BFS = (function() {
-        var directions = [ [1, 0], [0, 1], [-1, 0], [0, -1] ]
-
-        return function(row, col) {
-            $scope.matrix[row][col].visited = true
-
-            directions.forEach(function(direction, i) {
-                var nextRow = row + direction[0]
-                  , nextCol = col + direction[1]
-
-                if (isNext(nextRow, nextCol, $scope.matrix[row][col].value))
-                    $timeout(makeNext(nextRow, nextCol), FPS) // Make loop closure
-            })
-        }
     }())
 
-    // SCOPE
-    $scope.matrix = getSampleMatrix()
+    $scope.type = "DFS"
 
-    $scope.start = function(row, col) {
-        reset(), BFS(row, col)
-    }
+    $scope.currentLength = 0
+
+    $scope.animating = false
+
+    $scope.traverse = (function() {
+        var directions = [ [1, 0], [0, 1], [-1, 0], [0, -1] ]
+
+          , list = []
+
+        function reset() {
+            $scope.currentLength = 0
+
+            $scope.matrix.forEach(function(row) {
+                row.forEach(function(cell) {
+                    cell.visited = false
+                })
+            })
+        }
+
+        function isInside(row, col) {
+            return 0 <= row && row < $scope.matrix.length &&
+                   0 <= col && col < $scope.matrix[row].length
+        }
+
+        function isNext(row, col, value) {
+            if (!isInside(row, col))
+                return false
+
+            if ($scope.matrix[row][col].visited)
+                return false
+
+            if ($scope.matrix[row][col].value != value)
+                return false
+
+            return true
+        }
+
+        function visit(row, col) {
+            console.log(row, col)
+
+            $scope.currentLength++
+
+            $scope.matrix[row][col].visited = true
+        }
+
+        // Recursive loop with timeout
+        function loop(row, col) {
+            var cell = list[{ 'DFS': 'pop', 'BFS': 'shift' }[$scope.type]]() // Get the first or last element
+              , node = $scope.matrix[cell.row][cell.col]
+
+            if (!node.visited) {
+                visit(cell.row, cell.col)
+
+                directions.forEach(function(direction) {
+                    var nextRow = cell.row + direction[0]
+                      , nextCol = cell.col + direction[1]
+
+                    if (isNext(nextRow, nextCol, node.value))
+                        list.push({ row: nextRow, col: nextCol })
+                })
+            }
+
+            if (list.length) return $timeout(loop, FPS)
+
+            // TODO: Deferred
+            $timeout(function() {
+                $scope.animating = false
+            }, FPS)
+        }
+
+        return function(row, col) {
+            if ($scope.animating) return
+            else $scope.animating = true
+
+            reset()
+
+            list.push({ row: row, col: col })
+
+            loop()
+        }
+    }())
 })
 }())
