5385899585b4eb4ef60a95529dfd68b7d5941883
Jason Zhekov <jasssonpet@gmail.com>
2013-02-20 02:26:12 +0200

    Small fixes

--- matrix-traversal/scripts/script.js
+++ matrix-traversal/scripts/script.js
@@ -7,11 +7,11 @@ var APP = angular.module('app', [])
   , FPS = 1000 / 2 // Timeout delay
 
   , sampleMatrix =
-    [ [1, 3, 2, 2, 2, 4]
-    , [3, 3, 3, 2, 4, 4]
-    , [4, 3, 1, 2, 3, 3]
-    , [4, 3, 1, 3, 3, 1]
-    , [4, 3, 3, 3, 1, 1]
+        [ [1, 3, 2, 2, 2, 4]
+        , [3, 3, 3, 2, 4, 4]
+        , [4, 3, 1, 2, 3, 3]
+        , [4, 3, 1, 3, 3, 1]
+        , [4, 3, 3, 3, 1, 1]
     ]
 
 APP.controller('ctrl', function($scope, $timeout) {
